aea8726e78d765cb021dbd5c1704a12f1692aa70
Fernando Benitez-Paez <Fernando.Benitez@st-andrews.ac.uk>
2021-04-20 23:10:01 +0100

    minor clarifications

---
+++ StandAloneScript/MagGeoStandAloneScript.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import sys,os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import datetime, time
+from datetime import timedelta
+import math
+import pathlib
+from datetime import datetime
+import time
+import calendar
+import datetime
+from viresclient import ClientConfig
+import matplotlib.pyplot as plt
+from viresclient import set_token
+from MagGeoFunctions import getGPSData
+from MagGeoFunctions import Get_Swarm_residuals
+from MagGeoFunctions import ST_IDW_Process
+from MagGeoFunctions import CHAOS_ground_values
