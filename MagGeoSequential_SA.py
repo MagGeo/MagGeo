a7819bd0037f1c2b84cc196a1e0b645e94a87792
mfbenitezp <Fernando.Benitez@st-andrews.ac.uk>
2022-02-17 10:38:42 +0000

    Including parameters file

--- MagGeoSequential_SA.py
+++ MagGeoSequential_SA.py
@@ -1,9 +1,17 @@
+"""
+Core MagGeo model.
+Created on Thrus Feb 17, 22
+@author: Fernando Benitez-Paez
+"""
+
 import datetime as dt
 from datetime import timedelta
 import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import click
+from yaml import load, SafeLoader
 from viresclient import set_token
 from MagGeoFunctions import getGPSData
 from MagGeoFunctions import Get_Swarm_residuals
@@ -12,7 +20,7 @@ from MagGeoFunctions import CHAOS_ground_values
 
 
 set_token("https://vires.services/ows", set_default=True)
-os.chdir(r"./data")
+
 
 """""
 gpsfilename=input("What is the name of your .csv file?: ") # i.e BirdGPSTrajectory.csv
@@ -22,64 +30,90 @@ DateTime=input("Enter the date and time column name?: ") # i.e timestamp
 altitude = input("Enter the Altitude column name?, if you don't have the altitude column, just press Enter: ") 
 """""
 
-gpsfilename= "BirdGPSTrajectoryTest.csv"
-Lat="location-lat"
-Long="location-long"
-DateTime="timestamp"
-altitude = "height"
+@click.command()
+@click.option('-p',
+              '--parameters-file',
+              type=click.Path(exists=True),
+              help="Parameters file to use to configure the model. This must be located in the working directory.")
 
-GPSData = getGPSData(gpsfilename,Lat,Long,DateTime,altitude)
-os.chdir(r"../")
+def main(parameters_file):
+    """
+    Main function which runs the population initialisation, then chooses which model to run, either the Python/R
+    model or the OpenCL model
+    """
 
-datestimeslist = []
-for index, row in GPSData.iterrows():
-    datetimerow  = row['gpsDateTime']
-    daterow = row['dates']
-    hourrow = row['times']
-    hourrow = hourrow.strftime('%H:%M:%S')
-    if hourrow < '04:00:00':
-        date_bfr = daterow - (timedelta(days=1))
-        datestimeslist.append(daterow)
-        datestimeslist.append(date_bfr)
-    if hourrow > '20:00:00':
-        Date_aft = daterow + (timedelta(days=1))
-        datestimeslist.append(daterow)
-        datestimeslist.append(Date_aft)  
-    else:
-        datestimeslist.append(daterow)
+    print(f"--\nReading parameters file: {parameters_file}\n--")
 
-def uniquelistdates(list): 
-    x = np.array(list) 
-    uniquelist = np.unique(x)
-    return uniquelist
-uniquelist_dates = uniquelistdates(datestimeslist)
+    try:
+        with open(parameters_file, 'r') as f:
 
-hours_t_day = 24 #MagGeo needs the entire Swarm data for each day of the identified day.
-hours_added = dt.timedelta(hours = hours_t_day)
+            parameters = load(f,
+                              Loader=SafeLoader)
+            maggeo_params = parameters["maggeo"]  # Parameters for the dynamic microsim (python)
+            gpsfilename = maggeo_params["gpsfilename"]
+            Lat = maggeo_params["Lat"]
+            Long = maggeo_params["Long"] # Parameters for the disease model (r)
+            DateTime = maggeo_params["DateTime"]
+            altitude = maggeo_params["altitude"]
 
-listdfa = []
-listdfb = []
-listdfc = []
+    except Exception as error:
+        print('Error in parameters file format')
+        raise error
+    
+    
+    os.chdir(r"./data")
+    GPSData = getGPSData(gpsfilename,Lat,Long,DateTime,altitude)
+    os.chdir(r"../")
 
-for d in tqdm(uniquelist_dates, desc="Getting Swarm Data"):
-    #print("Getting Swarm data for date:",d )
-    startdate = dt.datetime.combine(d, dt.datetime.min.time())
-    enddate = startdate + hours_added
-    SwarmResidualsA,SwarmResidualsB,SwarmResidualsC = Get_Swarm_residuals(startdate, enddate)
-    listdfa.append(SwarmResidualsA)
-    listdfb.append(SwarmResidualsB)
-    listdfc.append(SwarmResidualsC)
+    datestimeslist = []
+    for index, row in GPSData.iterrows():
+        datetimerow  = row['gpsDateTime']
+        daterow = row['dates']
+        hourrow = row['times']
+        hourrow = hourrow.strftime('%H:%M:%S')
+        if hourrow < '04:00:00':
+            date_bfr = daterow - (timedelta(days=1))
+            datestimeslist.append(daterow)
+            datestimeslist.append(date_bfr)
+        if hourrow > '20:00:00':
+            Date_aft = daterow + (timedelta(days=1))
+            datestimeslist.append(daterow)
+            datestimeslist.append(Date_aft)  
+        else:
+            datestimeslist.append(daterow)
 
-os.chdir(r"./temp_data")
-TotalSwarmRes_A = pd.concat(listdfa, join='outer', axis=0)
-TotalSwarmRes_A.to_csv ('TotalSwarmRes_A.csv', header=True)
-TotalSwarmRes_B = pd.concat(listdfb, join='outer', axis=0)
-TotalSwarmRes_B.to_csv ('TotalSwarmRes_B.csv', header=True)
-TotalSwarmRes_C = pd.concat(listdfc, join='outer', axis=0)
-TotalSwarmRes_C.to_csv ('TotalSwarmRes_C.csv', header=True)
-os.chdir(r"../")
+    def uniquelistdates(list): 
+        x = np.array(list) 
+        uniquelist = np.unique(x)
+        return uniquelist
 
-if __name__ == '__main__':
+    uniquelist_dates = uniquelistdates(datestimeslist)
+
+    hours_t_day = 24 #MagGeo needs the entire Swarm data for each day of the identified day.
+    hours_added = dt.timedelta(hours = hours_t_day)
+
+    listdfa = []
+    listdfb = []
+    listdfc = []
+
+    for d in tqdm(uniquelist_dates, desc="Getting Swarm Data"):
+        #print("Getting Swarm data for date:",d )
+        startdate = dt.datetime.combine(d, dt.datetime.min.time())
+        enddate = startdate + hours_added
+        SwarmResidualsA,SwarmResidualsB,SwarmResidualsC = Get_Swarm_residuals(startdate, enddate)
+        listdfa.append(SwarmResidualsA)
+        listdfb.append(SwarmResidualsB)
+        listdfc.append(SwarmResidualsC)
+
+    os.chdir(r"./temp_data")
+    TotalSwarmRes_A = pd.concat(listdfa, join='outer', axis=0)
+    TotalSwarmRes_A.to_csv ('TotalSwarmRes_A.csv', header=True)
+    TotalSwarmRes_B = pd.concat(listdfb, join='outer', axis=0)
+    TotalSwarmRes_B.to_csv ('TotalSwarmRes_B.csv', header=True)
+    TotalSwarmRes_C = pd.concat(listdfc, join='outer', axis=0)
+    TotalSwarmRes_C.to_csv ('TotalSwarmRes_C.csv', header=True)
+    os.chdir(r"../")
+
     dn = [] ## List used to add all the GPS points with the annotated MAG Data. See the last bullet point of this process        
     for index, row in tqdm(GPSData.iterrows(), total=GPSData.shape[0], desc="Annotating the GPS Trayectory"):
         GPSLat = row['gpsLat']  
@@ -96,47 +130,46 @@ if __name__ == '__main__':
             result_badPoint= {'Latitude': GPSLat, 'Longitude': GPSLong, 'Altitude':GPSAltitude, 'DateTime': GPSDateTime, 'N_res': np.nan, 'E_res': np.nan, 'C_res':np.nan, 'TotalPoints':0, 'Minimum_Distance':np.nan, 'Average_Distance':np.nan}  
             dn.append(result_badPoint)
             continue
+    
+    os.chdir(r"./temp_data")
+    GPS_ResInt = pd.DataFrame(dn)
+    GPS_ResInt.to_csv ('GPS_ResInt.csv', header=True)
+    os.chdir(r"../")
 
+    X_obs, Y_obs, Z_obs, X_obs_internal, Y_obs_internal, Z_obs_internal = CHAOS_ground_values(GPS_ResInt)
 
+    GPS_ResInt['N'] =pd.Series(X_obs)
+    GPS_ResInt['E'] =pd.Series(Y_obs)
+    GPS_ResInt['C'] =pd.Series(Z_obs)
+    GPS_ResInt['N_Obs'] =pd.Series(X_obs_internal)
+    GPS_ResInt['E_Obs'] =pd.Series(Y_obs_internal)
+    GPS_ResInt['C_Obs'] =pd.Series(Z_obs_internal)
 
-os.chdir(r"./temp_data")
-GPS_ResInt = pd.DataFrame(dn)
-GPS_ResInt.to_csv ('GPS_ResInt.csv', header=True)
-os.chdir(r"../")
+    GPS_ResInt.drop(columns=['N_res', 'E_res','C_res'], inplace=True)
 
-X_obs, Y_obs, Z_obs, X_obs_internal, Y_obs_internal, Z_obs_internal = CHAOS_ground_values(GPS_ResInt)
+    # Having Intepolated and weighted the magnetic values, we can compute the other magnectic components. 
+    GPS_ResInt['H'] = np.sqrt((GPS_ResInt['N']**2)+(GPS_ResInt['E']**2))
+    #check the arcgtan in python., From arctan2 is saver.
+    DgpsRad = np.arctan2(GPS_ResInt['E'],GPS_ResInt['N'])
+    GPS_ResInt['D'] = np.degrees(DgpsRad)
+    IgpsRad = np.arctan2(GPS_ResInt['C'],GPS_ResInt['H'])
+    GPS_ResInt['I'] = np.degrees(IgpsRad)
+    GPS_ResInt['F'] = np.sqrt((GPS_ResInt['N']**2)+(GPS_ResInt['E']**2)+(GPS_ResInt['C']**2))
 
-GPS_ResInt['N'] =pd.Series(X_obs)
-GPS_ResInt['E'] =pd.Series(Y_obs)
-GPS_ResInt['C'] =pd.Series(Z_obs)
-GPS_ResInt['N_Obs'] =pd.Series(X_obs_internal)
-GPS_ResInt['E_Obs'] =pd.Series(Y_obs_internal)
-GPS_ResInt['C_Obs'] =pd.Series(Z_obs_internal)
+    os.chdir(r"./data")
+    originalGPSTrack=pd.read_csv(gpsfilename)
+    MagGeoResult = pd.concat([originalGPSTrack, GPS_ResInt], axis=1)
+    #Drop duplicated columns. Latitude, Longitued, and DateTime will not be part of the final result.
+    MagGeoResult.drop(columns=['Latitude', 'Longitude', 'DateTime'], inplace=True)
+    os.chdir(r"../")
 
-GPS_ResInt.drop(columns=['N_res', 'E_res','C_res'], inplace=True)
-GPS_ResInt
+    #Exporting the CSV file
+    os.chdir(r"./results")
+    outputfile ="GeoMagResult_"+gpsfilename
+    export_csv = MagGeoResult.to_csv (outputfile, index = None, header=True)
+    os.chdir(r"../")
+    print("Congrats! MagGeo has processed your GPS trayectory. Find the annotated table: " + outputfile + " in the folder results.")
 
-
-
-# Having Intepolated and weighted the magnetic values, we can compute the other magnectic components. 
-GPS_ResInt['H'] = np.sqrt((GPS_ResInt['N']**2)+(GPS_ResInt['E']**2))
-#check the arcgtan in python., From arctan2 is saver.
-DgpsRad = np.arctan2(GPS_ResInt['E'],GPS_ResInt['N'])
-GPS_ResInt['D'] = np.degrees(DgpsRad)
-IgpsRad = np.arctan2(GPS_ResInt['C'],GPS_ResInt['H'])
-GPS_ResInt['I'] = np.degrees(IgpsRad)
-GPS_ResInt['F'] = np.sqrt((GPS_ResInt['N']**2)+(GPS_ResInt['E']**2)+(GPS_ResInt['C']**2))
-
-os.chdir(r"./data")
-originalGPSTrack=pd.read_csv(gpsfilename)
-MagGeoResult = pd.concat([originalGPSTrack, GPS_ResInt], axis=1)
-#Drop duplicated columns. Latitude, Longitued, and DateTime will not be part of the final result.
-MagGeoResult.drop(columns=['Latitude', 'Longitude', 'DateTime'], inplace=True)
-os.chdir(r"../")
-
-#Exporting the CSV file
-os.chdir(r"./results")
-outputfile ="GeoMagResult_"+gpsfilename
-export_csv = MagGeoResult.to_csv (outputfile, index = None, header=True)
-os.chdir(r"../")
-print("Congrats! MagGeo has processed your GPS trayectory. Find the annotated table: " + outputfile + " in the folder results.")
+if __name__ == '__main__':
+    main()
+    print("End of MagGeo")
